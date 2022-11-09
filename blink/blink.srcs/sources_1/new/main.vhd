ad3a9aa17420e967d6f114f6aff9b03c6752e961
lonkaars <loek@pipeframe.xyz>
2022-11-09 14:16:43 +0100

    initial commit

---
+++ blink/blink.srcs/sources_1/new/main.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity main is
+	Port (clk : in STD_LOGIC;
+	      led : out STD_LOGIC);
+end main;
+
+architecture Behavioral of main is
+	signal count: STD_LOGIC_VECTOR(24 downto 0);
+begin
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			count <= (count + 1);
+		end if;
+	end process;
+	led <= count(24);
+
+end Behavioral;
---
+++ readme.md
@@ -0,0 +1,7 @@
+# programmeerbare hardware huiswerk
+
+hier staan de opdrachten voor programmeerbare hardware.
+
+ik weet niet of er een mogelijkheid is om fpga "code" te compileren en flashen
+met een makefile, maar als die er is zal dat misschien hier komen te staan,
+want ik vind vivado een beetje jakkie.
